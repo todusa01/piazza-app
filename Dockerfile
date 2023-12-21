965aacd5b24c7fca3082b16ac644fa7fab5602fa
todusa01 <odusamitemidire@gmail.com>
2023-12-21 14:08:13 +0100

    Pushing my app files

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM alpine
-RUN apk add --update nodejs npm
-COPY . /src
-WORKDIR /src
-EXPOSE 3000
-ENTRYPOINT ["node", "./app.js"]
--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UsersController = require('../controllers/usersController');
-
-router.post('/register', UsersController.register);
-router.post('/login', UsersController.login);
-
-module.exports = router;
